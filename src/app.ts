b0483464ead8b13159d42030f152bff2d80031ed
madrigal1 <kurupgokul11@gmail.com>
2020-12-19 21:55:49 +0530

    feat: div by 5 v4.0

--- src/app.ts
+++ src/app.ts
@@ -125,12 +125,12 @@ io.on('connection', async (socket: Socket) => {
 
     /** Check for a greater bid value */
     if (data.bid > currentBid) {
-      currentBid = data.bid;
       if(data.bid % 5 != 0) {
         socket.emit('invalid', { type: 'minimum', message: 'The bid you placed was not divisible by 5' });
         Logger.info(`The bid was not divisible by 5`);
         return;
       }
+      currentBid = data.bid;
       Logger.info(`${socket.id} made a bid of ${data.bid} (current bid: ${currentBid})`);
 
       /** Push bid to bid-history */
